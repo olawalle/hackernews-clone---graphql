a4d07530ee1671b06ecd9bdb85966241e0bb91d4
Olawale Ariyo <olawale.ariyo@cardinalstone.com>
2019-06-24 10:53:16 +0100

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Apollo } from 'apollo-angular'
+import gql from 'graphql-tag'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hckernews';
+  public news: []
+  public loaded: Boolean = false
+  constructor(apollo: Apollo) {
+    apollo
+      .query({
+        query: gql`
+          {
+            hn {
+              topStories(limit: 30, offset: 0) {
+                id,
+                by {
+                  id
+                },
+                timeISO, 
+                url,
+                title,
+                score,
+                descendants
+              }
+            }
+          }
+        `,
+      })
+      .subscribe(
+        res => {
+        this.loaded = true
+        this.news = res.data.hn.topStories
+      }, 
+        err => {
+        console.log({...err})
+      }
+    )
+  }
+
+  trim(url) {
+    let s = url.split('//')
+    let t = s[1].split('/')
+    return t[0].includes('www') ? t[0].split('.')[1]+'.'+t[0].split('.')[2] : t[0]
+  }
 }
